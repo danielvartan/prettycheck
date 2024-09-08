5ce395f929a4987e204d9743cd373c5efb0050a9
Daniel Vartanian <danvartan@gmail.com>
2024-09-08 19:31:17 -0300

    Add `assert_file` functions

--- R/file_exists.R
+++ R/file_exists.R
@@ -1,6 +1,11 @@
 # This is temporary!
 
-test_file_exists <- checkmate::test_file_exists
-check_file_exists <- checkmate::check_file_exists
-assert_file_exists <- checkmate::assert_file_exists
-expect_file_exists <- checkmate::expect_file_exists
+test_file <- checkmate::test_file_exists
+check_file <- checkmate::check_file_exists
+assert_file <- checkmate::assert_file_exists
+expect_file <- checkmate::expect_file_exists
+
+test_file_exists <- test_file
+check_file_exists <- check_file
+assert_file_exists <- assert_file
+expect_file_exists <- expect_file
