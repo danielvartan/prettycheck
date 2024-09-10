0b585a87302b1f11241d0f15fa54e92699fe1a26
Daniel Vartanian <danvartan@gmail.com>
2024-09-10 19:53:33 -0300

    Add `*_multi_class` functions

--- R/class.R
+++ R/class.R
@@ -4,3 +4,8 @@ test_class <- checkmate::test_class
 check_class <- checkmate::check_class
 assert_class <- checkmate::assert_class
 expect_class <- checkmate::expect_class
+
+test_multi_class <- checkmate::test_multi_class
+check_multi_class <- checkmate::check_multi_class
+assert_multi_class <- checkmate::assert_multi_class
+expect_multi_class <- checkmate::expect_multi_class
